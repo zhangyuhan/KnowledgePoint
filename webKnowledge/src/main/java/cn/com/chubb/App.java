90cacb2f918fdcb5e1b075af58f2b5eab0126442
Zhang Devin <zhang1988yuhan@163.com>
2019-08-29 15:36:32 +0800

    项目搭建

---
+++ webKnowledge/src/main/java/cn/com/chubb/App.java
@@ -0,0 +1,13 @@
+package cn.com.chubb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
